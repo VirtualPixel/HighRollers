93a4f7ba3e94700072fc4c82058937fb486ee4a1
Justin Wells <wellsmjustin@gmail.com>
2022-12-12 16:54:43 -0500

    Created die view

---
+++ HighRollers/Model/Shape.swift
@@ -0,0 +1,24 @@
+//
+//  Shape.swift
+//  HighRollers
+//
+//  Created by Justin Wells on 12/12/22.
+//
+
+import SwiftUI
+
+extension Shape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .stroke(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
+
+extension InsettableShape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .strokeBorder(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
--- HighRollers/View/Die.swift
+++ HighRollers/View/Die.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct Die: View {
+    let dieColor: [Color] = [Color(UIColor(red: 0.97, green: 0.96, blue: 0.93, alpha: 1.00)), Color(UIColor(red: 0.84, green: 0.81, blue: 0.69, alpha: 1.00)), Color(UIColor(red: 0.42, green: 0.38, blue: 0.27, alpha: 1.00))]
+    
     var body: some View {
-        GeometryReader { geo in
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(.red)
-                .frame(maxWidth: geo.size.width, maxHeight: .infinity)
-        }
+        Text("6")
+            .font(.system(size: 1000))
+            .minimumScaleFactor(0.005)
+            .frame(width: 400, height: 400)
+            .foregroundColor(.black)
+            .font(.title).bold()
+            .background(
+                LinearGradient(colors: dieColor, startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color(UIColor(red: 0.93, green: 0.90, blue: 0.81, alpha: 1.00)))
+                            .padding(30)
+                            .shadow(color: Color(UIColor(red: 0.93, green: 0.90, blue: 0.81, alpha: 1.00)), radius: 13)
+                            .shadow(color: Color(UIColor(red: 0.93, green: 0.90, blue: 0.81, alpha: 1.00)), radius: 0)
+                            .shadow(color: Color(UIColor(red: 0.93, green: 0.90, blue: 0.81, alpha: 1.00)), radius: 0)
+                            .shadow(color: Color(UIColor(red: 0.93, green: 0.90, blue: 0.81, alpha: 1.00)), radius: 0)
+                    )
+            )
+            .cornerRadius(16)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16).stroke(Color.black, lineWidth: 7)
+            )
     }
 }
 
